e7b2751a6866d504e0f8b83ed580f461159ebbda
Jonathan Watson <thejmdw@gmail.com>
2021-10-04 20:17:42 -0400

    user can log out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import { Appbar, Menu } from 'react-native-paper';
+import * as SecureStore from 'expo-secure-store';
+
+async function logout(key, value) {
+  await SecureStore.deleteItemAsync(key, value);
+}
 
 const MyComponent = ({navigation, back}) => {
   const _goBack = () => navigation.goBack;
@@ -34,6 +39,11 @@ const MyComponent = ({navigation, back}) => {
           <Menu.Item onPress={handleBurgers} title="Burgers" />
           <Menu.Item onPress={handleFries} title="Fries" />
           <Menu.Item onPress={() => {console.log('Option 3 was pressed')}} title="Option 3" disabled />
+          <Menu.Item onPress={() => {
+                                logout("dd_token")
+                                navigation.navigate("Login")
+                              }} 
+                      title="LogOut" />
         </Menu>
     </Appbar.Header>
   );
